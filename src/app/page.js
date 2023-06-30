58d1d7501be9a7740df90fe8151eb3152165d2c7
sebastian <sebastian@sisifospa.com>
2023-06-30 15:34:05 -0400

    Versión que saluda a Tomás y José

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
 
 return (
   <div>
-    <h1>Primer intento de tener algo corriendo live</h1>
+    <h1>Primer intento de tener algo corriendo live. Hola Tomás y José :)</h1>
   </div>
 );
 }
